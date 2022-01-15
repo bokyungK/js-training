47cbf19e9be5b049de1542e9f2ca052bad1949d2
bokyungK <freebob2b@gmail.com>
2022-01-15 23:51:19 +0900

    feat: Write basic rules about while

---
+++ while_quiz.js
@@ -0,0 +1,18 @@
+// while문 기본 문법
+
+  // const condition = true;
+  // let n = 0;
+
+  // 반복문 속 문장이 한 개인 경우
+  // while (condition)
+  //   console.log('Condition is true);
+
+
+  // 반복문 속 문장이 여러 개인 경우
+  // while (n < 10) {
+  //   n++
+  //   console.log(n);
+  // }
+
+
+// -----------------------------------------------
